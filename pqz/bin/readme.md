ce721dd80bb937c4de261966b9795f22a1c6516f
Giuliano Dedda <giuliano.dedda@yahoo.com>
2015-03-22 06:06:46 +0100

    piccole modifiche

--- pqz/bin/readme.md
+++ pqz/bin/readme.md
@@ -1,3 +1,5 @@
-List of programs
-===
-- genera-liste-da-ordinare.py : genera quiz con liste di numeri da ordinare. 
+= List of programs
+
+|====
+| genera-liste-da-ordinare.py 		| genera quiz con liste di numeri da ordinare. 
+| tabelline.py						| genera un file con le tabelline
---
+++ pqz/bin/tabelline.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+# gd 20150301
+"""
+Template for python
+"""
+
+
+import argparse
+import csv
+
+					
+
+
+
+def run():
+
+	fieldnames = ['id', 'question','correct_answer','wrong_answer','difficult_level','response_type','tags','if_correct','if_wrong']
+	with open('../csv/tabelline.csv', 'w') as csvfile:
+		writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+		writer.writeheader()
+		for i1 in range(0,11):
+			for i2 in range(0,11):
+				 writer.writerow({'question': str(i1)+' x '+str(i2), 'correct_answer': str(i1*i2)})
+		
+		
+def main():
+	
+	global args
+	parser = argparse.ArgumentParser(description='Genera il file csv con le tabelline')
+	args = parser.parse_args()
+
+	run()
+
+if __name__ == '__main__':
+    main()
+    
+
+
